e093d603408019800e8a742abcbe615e998dc49b
PhoonThetPine <thetpine254@gmail.com>
2023-10-04 15:24:40 +0800

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Phoon Thet Pine on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
